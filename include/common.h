c551d69f1f9d91e3cf0f6d209a70e2565398ae7b
SamSamhuns <sthapuskar@yahoo.com>
2019-04-02 17:29:29 +0400

    Added isdigit_all check function

--- include/common.h
+++ include/common.h
@@ -1,4 +1,9 @@
 #ifndef COMMON_H // Making sure the header files are not included twice
 #define COMMON_H
 
+
+/* Checks if the string str is all digits
+    returns 0 for True (str is all digits) and 1 for False (Not a number) */
+int isdigit_all (const char *str, int str_len);
+
 #endif
---
+++ src/common.c
@@ -0,0 +1,17 @@
+/*  Author: Samridha Shrestha
+    2019, April 2
+*/
+#include <ctype.h>
+#include <stdio.h>
+#include "common.h"
+
+/* Checks if the string str is all digits
+    returns 0 for True (str is all digits) and 1 for False (Not a number) */
+int isdigit_all (const char *str, int str_len) {
+	for (int j = 0; j < str_len; j++) {
+		if (isdigit(str[j]) == 0 ) {
+			return 1; /*Not a digit*/
+		}
+	}
+	return 0;
+}
