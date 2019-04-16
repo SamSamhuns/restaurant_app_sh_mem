ae61a26cb9cdc5a0eaf81952c57cb96e9e155daa
SamSamhuns <sthapuskar@yahoo.com>
2019-04-16 23:57:04 +0400

    Added test csv menu item loader sequence.

--- src/server.c
+++ src/server.c
@@ -10,12 +10,32 @@ int main(int argc, char const *argv[]) {
 	    ./server -m shmid */
 	if ( argc != 3 || strcmp(argv[1], "-m") != 0 || isdigit_all(argv[2], strlen(argv[2]))) {
 		fprintf(stderr,
-		        "Incorrect args supplied. Usage: ./server -m shmid(number)\n");
+		        "Incorrect args supplied. Usage: ./server -m shmid\n");
 		return 1;
 	}
 	long shmid;
 	shmid = strtol(argv[2], NULL, 10);
-	printf("DEBUG Long is %li \n", shmid);
+	FILE *menu_file = fopen("./db/diner_menu.txt", "r");
+	TRY_AND_CATCH(menu_file, "fopen_error");
+	if (DEBUG == 1) printf("DEBUG Long is %li \n", shmid);
+
+	// Create a Item struct array to hold each item from diner menu
+	struct Item menu_items[num_menu_items(menu_file)];
+
+	/* read the header */
+	char temp_hdr[MAX_ITEM_DESC_LEN];
+	fgets(temp_hdr, MAX_ITEM_DESC_LEN, menu_file);
+
+	load_item_struct_arr(menu_file, menu_items);
+	if (DEBUG) {
+		int temp_num = 0;
+		printf("%li %s %li %li %li\n", menu_items[temp_num].menu_itemId,
+		       menu_items[temp_num].menu_desc, menu_items[temp_num].menu_price,
+		       menu_items[temp_num].menu_min_time, menu_items[temp_num].menu_max_time
+		       );
+	}
+
+	fclose(menu_file);
 
 	/*TODO*/
 	// get shmid and access it
