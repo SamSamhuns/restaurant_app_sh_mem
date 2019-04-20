48562bf29bd1ab60b85e59324c7834d10dcdb32a
SamSamhuns <sthapuskar@yahoo.com>
2019-04-20 15:48:31 +0400

    added write_loc_sem, max_cli_cap, cli and serv queue structs

--- include/common.h
+++ include/common.h
@@ -3,11 +3,12 @@
 
 #define MAX_ITEM_DESC_LEN 125 // max len of menu items
 #define MAX_SHMID_LEN 30 // Max length of shmid
+#define MAX_REST_QUEUE_CAP 50 // Max size of our client and server queue, Max number of clients restaurant can handle in one run / day
 #define CASHIER_SEM "/cashier_sem" // named cashier semaphore
 #define CLIENTQ_SEM "/clientQ_sem" // amed client queue semaphore
+#define SHARED_MEM_WR_LOCK_SEM "/shared_mem_write_sem" // semaphore to lock the write segment
 
 #define DEBUG 1 // Debug Mode
-#define MAX_SHM_SIZE 4096 // max size of shared memory in bytes
 
 /* Boilerplate code for uniform error checking
   Example.  if(pipe(fd_pipe) == -1){
@@ -50,14 +51,60 @@ typedef struct Item {
 	long menu_max_time;
 } Item;
 
+typedef struct Client_CashQ_item {
+    pid_t client_pid;
+    int client_id;
+    int menu_itemId;
+} Client_CashQ_item;
+
+typedef struct Client_ServQ_item {
+    pid_t client_pid;
+    int client_id;
+    int menu_itemId;
+} Client_ServQ_item;
+
+/* This is the main  client record struct item */
+typedef struct Client_record_item {
+    pid_t clien_pid;
+    int client_id;
+    int menu_itemId;
+    char menu_desc[MAX_ITEM_DESC_LEN];
+    int menu_price;
+    int eat_time;
+    int time_with_cashier;
+    int time_with_server;
+} Client_record;
+
 /*Creating a shared memory struct*/
-typedef struct shared_memory_struct {
+typedef struct Shared_memory_struct {
+    /* We will deploy three custom array based queues with a
+        maximum capacity of MAX_REST_QUEUE_CAP (The maximum num of clients the
+        restaurant can handle )
+        They are client cashier queue, client server queue and a client record queue */
+    int front_client_Q;
+    int rear_client_Q;
+    int size_client_Q;
+    struct Client_CashQ_item client_cash_queue[MAX_REST_QUEUE_CAP];
+
+    int front_server_Q;
+    int rear_server_Q;
+    int size_server_Q;
+    struct Client_ServQ_item client_server_queue[MAX_REST_QUEUE_CAP];
+
+    /* client_record_cur_size cannot exceed MAX_REST_QUEUE_CAP */
+    int client_record_cur_size;
+    struct Client_record_item client_record_queue[MAX_REST_QUEUE_CAP];
+
+    /* static constants */
     int MaxCashiers;
     int MaxPeople;
-    int closeRestaurant;
-    int totalCashierNum;
-    int totalClientNum;
-} shared_memory_struct;
+
+    /* dynamic values */
+    pid_t server_pid; // pid of current server program
+    int totalCashierNum; // current number of cashieers
+    int totalClientNum; // current number of total clients
+    int totalClientOverall; // overall number of clients processed must be less than MaxPeople
+} Shared_memory_struct;
 
 /* Checks if the string str is all digits
     returns 0 for True (str is all digits) and 1 for False (Not a number) */
