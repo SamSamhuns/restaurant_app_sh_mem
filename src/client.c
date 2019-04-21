050a9abe842e5ae3ed018944f5285be80a6a3189
SamSamhuns <sthapuskar@yahoo.com>
2019-04-21 15:44:58 +0400

    Open new sems and correct names

--- src/client.c
+++ src/client.c
@@ -46,31 +46,41 @@ int main(int argc, char const *argv[]){
 	fclose(menu_file);
 
 	////////////////* ACCESS THE SHARED MEMORY STRUCTURE *//////////////////////
-	sem_t *clientQS = sem_open(CLIENTQ_SEM, 0); /* open existing clientQS semaphore */
-	sem_t *cashierS = sem_open(CASHIER_SEM, 0); /* open existing cashierS semaphore */
-	sem_t *shared_mem_write_sem = sem_open(SHARED_MEM_WR_LOCK_SEM, 0); /* open existing shared_mem_write_sem semaphore */
-	TRY_AND_CATCH_SEM(clientQS, "sem_open()");
-	TRY_AND_CATCH_SEM(cashierS, "sem_open()");
-	TRY_AND_CATCH_SEM(shared_mem_write_sem, "sem_open()");
+	sem_t *cashier_sem = sem_open(CASHIER_SEM, 0); /* open existing server_sem semaphore */
+	sem_t *cashier_cli_q_sem = sem_open(CASHIER_CLI_Q_SEM, 0); /* open existing server_cli_q_sem semaphore */
+	sem_t *deq_c_block_sem = sem_open(DEQ_C_BLOCK_SEM, 0); /* open existing deq_s_block_sem semaphore */
+	sem_t *server_sem = sem_open(SERVER_SEM, 0); /* open existing server_sem semaphore */
+	sem_t *server_cli_q_sem = sem_open(SERVER_CLI_Q_SEM, 0); /* open existing server_cli_q_sem semaphore */
+	sem_t *deq_s_block_sem = sem_open(DEQ_S_BLOCK_SEM, 0); /* open existing deq_s_block_sem semaphore */
+	sem_t *shm_write_sem = sem_open(SHM_WRITE_SEM, 0); /* open existing server_sem semaphore */
+	sem_t *shutdown_sem = sem_open(SHUTDOWN_SEM, 0); /* open existing server_sem semaphore */
+	TRY_AND_CATCH_SEM(cashier_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(cashier_cli_q_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(deq_c_block_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(server_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(server_cli_q_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(deq_s_block_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(shm_write_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(shutdown_sem, "sem_open()");
 
 	/* shared memory file descriptor */
 	int shm_fd;
-	struct Shared_memory_struct *shared_mem_ptr;
+	struct Shared_memory_struct *shm_ptr;
 
 	/* open the shared memory object */
 	shm_fd = shm_open(shmid, O_RDWR, 0666);
 	TRY_AND_CATCH_INT(shm_fd, "shm_open()");
 
 	/* memory map the shared memory object */
-	if ((shared_mem_ptr = mmap(0, sizeof(Shared_memory_struct),
+	if ((shm_ptr = mmap(0, sizeof(Shared_memory_struct),
 	                           PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED) {
 		perror("mmap");
 		exit(1);
 	};
 
 	/* read from the shared memory object */
-	printf("DEBUG The maxCashier number is %i\n",  shared_mem_ptr->MaxCashiers);
-	printf("DEBUG The maxPeople number is %i\n", shared_mem_ptr->MaxPeople);
+	printf("DEBUG The maxCashier number is %i\n",  shm_ptr->MaxCashiers);
+	printf("DEBUG The maxPeople number is %i\n", shm_ptr->MaxPeople);
 
 	////////////////////////////////////////////////////////////////////////////
 	////////////////////* Main section in Client begins *///////////////////////
@@ -78,22 +88,22 @@ int main(int argc, char const *argv[]){
 	int client_not_enter_restaurant = 0;
 
 	/* if restaurant is not properly staffed */
-	if (shared_mem_ptr->cur_cashier_num == 0 || shared_mem_ptr->server_pid == NO_SERVER_TEMP_PID) {
+	if (shm_ptr->cur_cashier_num == 0 || shm_ptr->server_pid == NO_SERVER_TEMP_PID) {
 		printf("Client will leave because restaurant is not properly staffed\n");
 		client_not_enter_restaurant = 1;
 	}
 	/* If the current num of clients being served in the restaurant has reached MaxPeople limit */
-	else if (shared_mem_ptr->cur_client_num+1 > shared_mem_ptr->MaxPeople) {
+	else if (shm_ptr->cur_client_num+1 > shm_ptr->MaxPeople) {
 		printf("Client will not be joining restaurant queue as it is full right now\n");
 		client_not_enter_restaurant = 1;
 	}
 	/* If the overall number of clients has exceeded the MAX_REST_QUEUE_CAP restaurant global capacity */
-	else if (shared_mem_ptr->overall_client_num+1 > MAX_REST_QUEUE_CAP) {
+	else if (shm_ptr->overall_client_num+1 > MAX_REST_QUEUE_CAP) {
 		printf("Client will leave because restaurant has reached its capacity and will not take any more clients today\n");
 		client_not_enter_restaurant = 1;
 	}
 	/* if shutting down has been initiated by the coordinator */
-	else if (shared_mem_ptr->initiate_shutdown == 1) {
+	else if (shm_ptr->initiate_shutdown == 1) {
 		printf("Client will leave because restaurant is going to shut down now\n");
 		client_not_enter_restaurant = 1;
 	}
@@ -101,39 +111,41 @@ int main(int argc, char const *argv[]){
 
 	if (client_not_enter_restaurant) {
 		/* Client will not enter restaurant so clean up normally and exit */
-		all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
+		all_exit_cleanup(cashier_sem, cashier_cli_q_sem, deq_c_block_sem, server_sem,
+						 server_cli_q_sem, deq_s_block_sem, shm_write_sem, shutdown_sem,
+						 shm_ptr, &shm_fd);
 		return 0;
 	}
 
 	/* If control reaches here Now we can increment cur_client_num, overall_client_num
 	        and add clients first to the client_cashier_queue */
 	////////* Acquire semaphore lock first before writing in shared memory *//////
-	if (sem_wait(shared_mem_write_sem) == -1) {
+	if (sem_wait(shm_write_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
-	shared_mem_ptr->cur_client_num += 1;
-	shared_mem_ptr->overall_client_num += 1;
+	shm_ptr->cur_client_num += 1;
+	shm_ptr->overall_client_num += 1;
 
 	/* release semaphore write lock after writing to shared memory */
-	if (sem_post(shared_mem_write_sem) == -1) {
+	if (sem_post(shm_write_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
 	//////////////////////////////////////////////////////////////////////////////
 
-	////////* Client locks the clientQS semaphore by calling wait on it */////////
+	////////* Client locks the cashier_cli_q_sem semaphore by calling wait on it */////////
 	//////////////////////////////////////////////////////////////////////////////
-	if (sem_wait(clientQS) == -1) {
+	if (sem_wait(cashier_cli_q_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
 
 	/* Client joins the client_cashier_queue */
-	enqueue_client_cashier_q(shared_mem_ptr, itemId, shared_mem_write_sem);
+	enqueue_client_cashier_q(shm_ptr, itemId, shm_write_sem);
 
 	/* signal sempahore after adding itself to the client_cashier_queue */////////
-	if (sem_post(clientQS) == -1) {
+	if (sem_post(cashier_cli_q_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
@@ -143,15 +155,15 @@ int main(int argc, char const *argv[]){
 	/* Client waits till cashier has set a serving time for both */
 	int temp_breaker = 1;
 	while (temp_breaker) {
-		for (int i = shared_mem_ptr->front_client_Q; i < shared_mem_ptr->cur_client_record_size; i++) {
-			if ( (shared_mem_ptr->client_record_array[i]).client_pid == getpid() ) {
+		for (int i = shm_ptr->front_client_Q; i < shm_ptr->cur_client_record_size; i++) {
+			if ( (shm_ptr->client_record_array[i]).client_pid == getpid() ) {
 				printf("Being served by a cashier right now\n");
 				temp_breaker = 0;
 				break;
 			}
 		}
 	}
-	sleep((shared_mem_ptr->client_record_array[shared_mem_ptr->cur_client_record_size]).time_with_cashier); /* client is being serve by the cashier */
+	sleep((shm_ptr->client_record_array[shm_ptr->cur_client_record_size]).time_with_cashier); /* client is being serve by the cashier */
 
 	/*TODO, get order with server*/
 
@@ -166,9 +178,9 @@ int main(int argc, char const *argv[]){
 	}
 	printf("Client %li is eating for %i s before leaving\n", (long)getpid(), temp_sleep_time );
 	/* setting the eat time for the client */
-	for (int i = 0; i < shared_mem_ptr->cur_client_record_size; i++) {
-		if ( (shared_mem_ptr->client_record_array[i]).client_pid == getpid() ) {
-			(shared_mem_ptr->client_record_array[i]).eat_time = temp_sleep_time;
+	for (int i = 0; i < shm_ptr->cur_client_record_size; i++) {
+		if ( (shm_ptr->client_record_array[i]).client_pid == getpid() ) {
+			(shm_ptr->client_record_array[i]).eat_time = temp_sleep_time;
 			break;
 		}
 	}
@@ -177,14 +189,14 @@ int main(int argc, char const *argv[]){
 
 	/* Decrement the cur_client_num counter before leaving */
 	////////* Acquire semaphore lock first before writing in shared memory *//////
-	if (sem_wait(shared_mem_write_sem) == -1) {
+	if (sem_wait(shm_write_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
-	shared_mem_ptr->cur_client_num -= 1;
+	shm_ptr->cur_client_num -= 1;
 
 	/* release semaphore write lock after writing to shared memory */
-	if (sem_post(shared_mem_write_sem) == -1) {
+	if (sem_post(shm_write_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
@@ -192,7 +204,9 @@ int main(int argc, char const *argv[]){
 
 	printf("Client with ID %i has successfully ordered, dined and left the restaurant\n", getpid());
 	/* Clean up normally */
-	all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
+	all_exit_cleanup(cashier_sem, cashier_cli_q_sem, deq_c_block_sem, server_sem,
+					 server_cli_q_sem, deq_s_block_sem, shm_write_sem, shutdown_sem,
+					 shm_ptr, &shm_fd);
 	return 0;
 }
 
