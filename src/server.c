e217deacec357e3c30596d744c4235cb30f2e1c2
SamSamhuns <sthapuskar@yahoo.com>
2019-04-21 04:04:37 +0400

    Add uncrustify edits

--- src/server.c
+++ src/server.c
@@ -65,26 +65,26 @@ int main(int argc, char const *argv[]) {
 	/* Check if more than one servers are being tried to be initiated */
 	/* add Server pid to the server pid var in shared mem */
 	if (shared_mem_ptr->server_pid == NO_SERVER_TEMP_PID) {
-			////////* Acquire semaphore lock first before writing in shared memory *//////
-			if (sem_wait(shared_mem_write_sem) == -1) {																	//
-				perror("sem_wait()");																											//
-				exit(1);																																	//
-			}																																						//
-			shared_mem_ptr->server_pid = getpid();																			//
-																																									//
-			/* release semaphore write lock after writing to shared memory */						//
-			if (sem_post(shared_mem_write_sem) == -1) {																	//
-				perror("sem_post()");																											//
-				exit(1);																																	//
-			}																																						//
-			//////////////////////////////////////////////////////////////////////////////
+		////////* Acquire semaphore lock first before writing in shared memory *////
+		if (sem_wait(shared_mem_write_sem) == -1) {                               //
+			perror("sem_wait()");                                                 //
+			exit(1);                                                              //
+		}                                                                         //
+		shared_mem_ptr->server_pid = getpid();                                    //
+																				  //
+		/* release semaphore write lock after writing to shared memory */         //
+		if (sem_post(shared_mem_write_sem) == -1) {                               //
+			perror("sem_post()");                                                 //
+			exit(1);                                                              //
+		}                                                                         //
+		////////////////////////////////////////////////////////////////////////////
 
 	}
 	else {
-			printf("Only one server is allowed in the restaurant\n");
-			/* Clean up normally */
-			all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
-			return 0;
+		printf("Only one server is allowed in the restaurant\n");
+		/* Clean up normally */
+		all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
+		return 0;
 	}
 	////////////////////////////////////////////////////////////////////////////
 	/////////////////* Main while loop in Cashier begins *//////////////////////
