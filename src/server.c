8fd1e6a030c616ce942c3456c23507e518895bcf
SamSamhuns <sthapuskar@yahoo.com>
2019-04-21 15:45:31 +0400

    Add server exit cleanup funcs

--- src/server.c
+++ src/server.c
@@ -30,50 +30,60 @@ int main(int argc, char const *argv[]) {
 	fclose(menu_file);
 
 	////////////////* ACCESS THE SHARED MEMORY STRUCTURE *//////////////////////
-	sem_t *clientQS = sem_open(CLIENTQ_SEM, 0); /* open existing clientQS semaphore */
-	sem_t *cashierS = sem_open(CASHIER_SEM, 0); /* open existing cashierS semaphore */
-	sem_t *shared_mem_write_sem = sem_open(SHARED_MEM_WR_LOCK_SEM, 0); /* open existing shared_mem_write_sem semaphore */
-	TRY_AND_CATCH_SEM(clientQS, "sem_open()");
-	TRY_AND_CATCH_SEM(cashierS, "sem_open()");
-	TRY_AND_CATCH_SEM(shared_mem_write_sem, "sem_open()");
+	sem_t *server_sem = sem_open(SERVER_SEM, 0); /* open existing server_sem semaphore */
+	sem_t *server_cli_q_sem = sem_open(SERVER_CLI_Q_SEM, 0); /* open existing server_cli_q_sem semaphore */
+	sem_t *deq_s_block_sem = sem_open(DEQ_S_BLOCK_SEM, 0); /* open existing deq_s_block_sem semaphore */
+	sem_t *shm_write_sem = sem_open(SHM_WRITE_SEM, 0); /* open existing server_sem semaphore */
+	sem_t *shutdown_sem = sem_open(SHUTDOWN_SEM, 0); /* open existing server_sem semaphore */
+	TRY_AND_CATCH_SEM(server_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(server_cli_q_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(deq_s_block_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(shm_write_sem, "sem_open()");
+	TRY_AND_CATCH_SEM(shutdown_sem, "sem_open()");
 
 	/* shared memory file descriptor */
 	int shm_fd;
-	struct Shared_memory_struct *shared_mem_ptr;
+	struct Shared_memory_struct *shm_ptr;
 
 	/* open the shared memory object */
 	shm_fd = shm_open(argv[2], O_RDWR, 0666);
 	TRY_AND_CATCH_INT(shm_fd, "shm_open()");
 
 	/* memory map the shared memory object */
-	if ((shared_mem_ptr = mmap(0, sizeof(Shared_memory_struct),
-	                           PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED) {
+	if ((shm_ptr = mmap(0, sizeof(Shared_memory_struct),
+	                    PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED) {
 		perror("mmap");
 		exit(1);
 	};
 
 	/* read from the shared memory object */
-	printf("DEBUG The maxCashier number is %i\n",  shared_mem_ptr->MaxCashiers);
-	printf("DEBUG The maxPeople number is %i\n", shared_mem_ptr->MaxPeople);
+	printf("DEBUG The maxCashier number is %i\n",  shm_ptr->MaxCashiers);
+	printf("DEBUG The maxPeople number is %i\n", shm_ptr->MaxPeople);
 
 	/* if shutting down has already been initiated by the coordinator */
-	if (shared_mem_ptr->initiate_shutdown == 1) {
+	if (shm_ptr->initiate_shutdown == 1) {
 		/* Clean up normally */
-		all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
+		server_exit_cleanup(server_sem,
+							server_cli_q_sem,
+							deq_s_block_sem,
+							shm_write_sem,
+							shutdown_sem,
+							shm_ptr,
+							&shm_fd);
 		return 0;
 	}
 	/* Check if more than one servers are being tried to be initiated */
 	/* add Server pid to the server pid var in shared mem */
-	if (shared_mem_ptr->server_pid == NO_SERVER_TEMP_PID) {
+	if (shm_ptr->server_pid == NO_SERVER_TEMP_PID) {
 		////////* Acquire semaphore lock first before writing in shared memory *////
-		if (sem_wait(shared_mem_write_sem) == -1) {                               //
+		if (sem_wait(shm_write_sem) == -1) {                               //
 			perror("sem_wait()");                                                 //
 			exit(1);                                                              //
 		}                                                                         //
-		shared_mem_ptr->server_pid = getpid();                                    //
-																				  //
+		shm_ptr->server_pid = getpid();                                    //
+		//
 		/* release semaphore write lock after writing to shared memory */         //
-		if (sem_post(shared_mem_write_sem) == -1) {                               //
+		if (sem_post(shm_write_sem) == -1) {                               //
 			perror("sem_post()");                                                 //
 			exit(1);                                                              //
 		}                                                                         //
@@ -83,7 +93,13 @@ int main(int argc, char const *argv[]) {
 	else {
 		printf("Only one server is allowed in the restaurant\n");
 		/* Clean up normally */
-		all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
+		server_exit_cleanup(server_sem,
+							server_cli_q_sem,
+							deq_s_block_sem,
+							shm_write_sem,
+							shutdown_sem,
+							shm_ptr,
+							&shm_fd);
 		return 0;
 	}
 	////////////////////////////////////////////////////////////////////////////
@@ -92,13 +108,19 @@ int main(int argc, char const *argv[]) {
 
 	while (1) {
 		/* if shutting down has been initiated by the coordinator */
-		if (shared_mem_ptr->initiate_shutdown == 1) {
+		if (shm_ptr->initiate_shutdown == 1) {
 			/* Clean up normally */
-			all_exit_cleanup(clientQS, cashierS, shared_mem_write_sem, shared_mem_ptr, &shm_fd);
+			server_exit_cleanup(server_sem,
+			                    server_cli_q_sem,
+			                    deq_s_block_sem,
+			                    shm_write_sem,
+			                    shutdown_sem,
+			                    shm_ptr,
+			                    &shm_fd);
 			return 0;
 		}
 
-		
+
 
 		/*TODO handle the server queue */
 	}
