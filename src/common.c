49c547d344c6cc851fb610a48f166dfab0f28a9d
SamSamhuns <sthapuskar@yahoo.com>
2019-04-21 15:44:08 +0400

    Create new server and shutdown sem with name changes for existing sems

--- src/common.c
+++ src/common.c
@@ -61,33 +61,94 @@ void load_item_struct_arr(FILE *menu_file, struct Item menu_items[]) {
 /* FINAL CLEAN UP function for unlinking shm and sem
     should only be called in the coordinator */
 void coordinator_only_exit_cleanup() {
-	/* remove the named semaphores
-	    IMPORTANT only coordinator should call this
-	    at the end with error handling */
+	/* remove the named semaphores*/
 	TRY_AND_CATCH_INT(sem_unlink(CASHIER_SEM), "sem_unlink");
-	TRY_AND_CATCH_INT(sem_unlink(CLIENTQ_SEM), "sem_unlink");
-	TRY_AND_CATCH_INT(sem_unlink(SHARED_MEM_WR_LOCK_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(CASHIER_CLI_Q_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(DEQ_C_BLOCK_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(SERVER_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(SERVER_CLI_Q_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(DEQ_S_BLOCK_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(SHUTDOWN_SEM), "sem_unlink");
+	TRY_AND_CATCH_INT(sem_unlink(SHM_WRITE_SEM), "sem_unlink");
 
-	/* remove the shared mem object
-	    IMPORTANT only coordinator should call this
-	    at the end with error handling */
+	/* remove the shared mem object */
 	TRY_AND_CATCH_INT(shm_unlink(SHMID), "shm_unlink");
 }
 
-/* Normal exit clean up call for all processes */
-void all_exit_cleanup(sem_t *clientQS,
-                      sem_t *cashierS,
-                      sem_t *shared_mem_write_sem,
-                      struct Shared_memory_struct *shared_mem_ptr,
+/* Normal exit semaphore close and mem unmap call
+    This will be called only by the coordinator and the client since they use
+    all semaphores */
+void all_exit_cleanup(sem_t *cashier_sem,
+                      sem_t *cashier_cli_q_sem,
+                      sem_t *deq_c_block_sem,
+                      sem_t *server_sem,
+                      sem_t *server_cli_q_sem,
+                      sem_t *deq_s_block_sem,
+                      sem_t *shm_write_sem,
+                      sem_t *shutdown_sem,
+                      struct Shared_memory_struct *shm_ptr,
                       int *shm_fd) {
 	/* close the named semaphores with error handling */
-	TRY_AND_CATCH_INT(sem_close(clientQS), "sem_close");
-	TRY_AND_CATCH_INT(sem_close(cashierS), "sem_close");
-	TRY_AND_CATCH_INT(sem_close(shared_mem_write_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(cashier_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(cashier_cli_q_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(deq_c_block_sem), "sem_close");
+
+	TRY_AND_CATCH_INT(sem_close(server_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(server_cli_q_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(deq_s_block_sem), "sem_close");
+
+	TRY_AND_CATCH_INT(sem_close(shm_write_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(shutdown_sem), "sem_close");
 
 	/* remove the shared memory object with error handling */
 	TRY_AND_CATCH_INT(
-		munmap(shared_mem_ptr, sizeof(Shared_memory_struct)), "munmap()");
+		munmap(shm_ptr, sizeof(Shared_memory_struct)), "munmap()");
+	close(*shm_fd);
+	printf("Successfully exiting process with pid %li\n", (long)getpid());
+}
+
+/* Normal exit sempaphore and shm cleanup for cashier and the sems it uses */
+void cashier_exit_cleanup(sem_t *cashier_sem,
+                          sem_t *cashier_cli_q_sem,
+                          sem_t *deq_c_block_sem,
+                          sem_t *shm_write_sem,
+                          sem_t *shutdown_sem,
+                          struct Shared_memory_struct *shm_ptr,
+                          int *shm_fd) {
+	/* close the named semaphores with error handling */
+	TRY_AND_CATCH_INT(sem_close(cashier_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(cashier_cli_q_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(deq_c_block_sem), "sem_close");
+
+	TRY_AND_CATCH_INT(sem_close(shm_write_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(shutdown_sem), "sem_close");
+
+	/* remove the shared memory object with error handling */
+	TRY_AND_CATCH_INT(
+		munmap(shm_ptr, sizeof(Shared_memory_struct)), "munmap()");
+	close(*shm_fd);
+	printf("Successfully exiting process with pid %li\n", (long)getpid());
+}
+
+/* Normal exit sempaphore and shm cleanup for server and the sems it uses */
+void server_exit_cleanup(sem_t *server_sem,
+                         sem_t *server_cli_q_sem,
+                         sem_t *deq_s_block_sem,
+                         sem_t *shm_write_sem,
+                         sem_t *shutdown_sem,
+                         struct Shared_memory_struct *shm_ptr,
+                         int *shm_fd) {
+	/* close the named semaphores with error handling */
+	TRY_AND_CATCH_INT(sem_close(server_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(server_cli_q_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(deq_s_block_sem), "sem_close");
+
+	TRY_AND_CATCH_INT(sem_close(shm_write_sem), "sem_close");
+	TRY_AND_CATCH_INT(sem_close(shutdown_sem), "sem_close");
+
+	/* remove the shared memory object with error handling */
+	TRY_AND_CATCH_INT(
+		munmap(shm_ptr, sizeof(Shared_memory_struct)), "munmap()");
 	close(*shm_fd);
 	printf("Successfully exiting process with pid %li\n", (long)getpid());
 }
@@ -95,32 +156,32 @@ void all_exit_cleanup(sem_t *clientQS,
 /* func to enqueue client pid in cashier FIFO queue
     This function automatically does a shared mem write semaphore lock
     IMPORTANT: This function should NEVER be called INSIDE the shared mem write semaphore lock state */
-void enqueue_client_cashier_q(struct Shared_memory_struct *shared_mem_ptr,
-                              long menu_item_id, sem_t *shared_mem_write_sem) {
+void enqueue_client_cashier_q(struct Shared_memory_struct *shm_ptr,
+                              long menu_item_id, sem_t *shm_write_sem) {
 	////////* Acquire semaphore lock first before writing in shared memory *////
 	////////////////////////////////////////////////////////////////////////////
-	if (sem_wait(shared_mem_write_sem) == -1) {
+	if (sem_wait(shm_write_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
 	/* if no clients have been processed before in the cashier queue */
-	if (shared_mem_ptr->size_client_Q < 0) {
-		shared_mem_ptr->client_cashier_queue[0].client_pid = getpid();
-		shared_mem_ptr->client_cashier_queue[0].menu_item_id = (int)menu_item_id;
-		shared_mem_ptr->front_client_Q = 0;         // front = 0
-		shared_mem_ptr->rear_client_Q = 0;         // rear = 0
-		shared_mem_ptr->size_client_Q = 1;         // size  = 1
+	if (shm_ptr->size_client_Q < 0) {
+		shm_ptr->client_cashier_queue[0].client_pid = getpid();
+		shm_ptr->client_cashier_queue[0].menu_item_id = (int)menu_item_id;
+		shm_ptr->front_client_Q = 0;         // front = 0
+		shm_ptr->rear_client_Q = 0;         // rear = 0
+		shm_ptr->size_client_Q = 1;         // size  = 1
 	}
 	/* if clients have been previously processed in the cashier queue */
 	else {
-		int rear = shared_mem_ptr->rear_client_Q;
-		shared_mem_ptr->client_cashier_queue[rear+1].client_pid = getpid();
-		shared_mem_ptr->client_cashier_queue[rear+1].menu_item_id = (int)menu_item_id;
-		shared_mem_ptr->rear_client_Q++;       // rear ++
-		shared_mem_ptr->size_client_Q++;       // size ++
+		int rear = shm_ptr->rear_client_Q;
+		shm_ptr->client_cashier_queue[rear+1].client_pid = getpid();
+		shm_ptr->client_cashier_queue[rear+1].menu_item_id = (int)menu_item_id;
+		shm_ptr->rear_client_Q++;       // rear ++
+		shm_ptr->size_client_Q++;       // size ++
 	}
 	/* release semaphore write lock after writing to shared memory */
-	if (sem_post(shared_mem_write_sem) == -1) {
+	if (sem_post(shm_write_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
@@ -130,34 +191,34 @@ void enqueue_client_cashier_q(struct Shared_memory_struct *shared_mem_ptr,
 /* func to enqueue client pid in server FIFO queue
    This function automatically does a shared mem write semaphore lock
    IMPORTANT: This function should NEVER be called INSIDE the shared mem write semaphore lock state */
-void enqueue_client_server_q(struct Shared_memory_struct *shared_mem_ptr,
-                             long menu_item_id, sem_t *shared_mem_write_sem) {
+void enqueue_client_server_q(struct Shared_memory_struct *shm_ptr,
+                             long menu_item_id, sem_t *shm_write_sem) {
 	////////* Acquire semaphore lock first before writing in shared memory *////
 	////////////////////////////////////////////////////////////////////////////
-	if (sem_wait(shared_mem_write_sem) == -1) {
+	if (sem_wait(shm_write_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
 	/* if no clients have been processed before in the cashier queue */
-	if (shared_mem_ptr->size_server_Q < 0) {
+	if (shm_ptr->size_server_Q < 0) {
 		/* Add client pid and order info to queue */
-		shared_mem_ptr->client_server_queue[0].client_pid = getpid();
-		shared_mem_ptr->client_server_queue[0].menu_item_id = (int)menu_item_id;
-		shared_mem_ptr->front_server_Q = 0;         // front = 0
-		shared_mem_ptr->rear_server_Q = 0;         // rear = 0
-		shared_mem_ptr->size_server_Q = 1;         // size  = 1
+		shm_ptr->client_server_queue[0].client_pid = getpid();
+		shm_ptr->client_server_queue[0].menu_item_id = (int)menu_item_id;
+		shm_ptr->front_server_Q = 0;         // front = 0
+		shm_ptr->rear_server_Q = 0;         // rear = 0
+		shm_ptr->size_server_Q = 1;         // size  = 1
 	}
 	/* if clients have been previously processed in the cashier queue */
 	else {
-		int rear = shared_mem_ptr->rear_server_Q;
+		int rear = shm_ptr->rear_server_Q;
 		/* Add client pid and order info to queue */
-		shared_mem_ptr->client_server_queue[rear+1].client_pid = getpid();
-		shared_mem_ptr->client_server_queue[rear+1].menu_item_id = (int)menu_item_id;
-		shared_mem_ptr->rear_server_Q++;       // rear ++
-		shared_mem_ptr->size_server_Q++;       // size ++
+		shm_ptr->client_server_queue[rear+1].client_pid = getpid();
+		shm_ptr->client_server_queue[rear+1].menu_item_id = (int)menu_item_id;
+		shm_ptr->rear_server_Q++;       // rear ++
+		shm_ptr->size_server_Q++;       // size ++
 	}
 	/* release semaphore write lock after writing to shared memory */
-	if (sem_post(shared_mem_write_sem) == -1) {
+	if (sem_post(shm_write_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
@@ -167,22 +228,22 @@ void enqueue_client_server_q(struct Shared_memory_struct *shared_mem_ptr,
 /* func to dequeue client pid item in client FIFO queue
    This function automatically does a shared mem write semaphore lock
    IMPORTANT: This function should NEVER be called INSIDE the shared mem write semaphore lock state */
-void dequeue_client_cashier_q(struct Shared_memory_struct *shared_mem_ptr, sem_t *shared_mem_write_sem) {
+void dequeue_client_cashier_q(struct Shared_memory_struct *shm_ptr, sem_t *shm_write_sem) {
 	////////* Acquire semaphore lock first before writing in shared memory *////
 	////////////////////////////////////////////////////////////////////////////
-	if (sem_wait(shared_mem_write_sem) == -1) {
+	if (sem_wait(shm_write_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
-	if (shared_mem_ptr->size_client_Q > 0) {
-		shared_mem_ptr->size_client_Q--;      // size --
-		shared_mem_ptr->front_client_Q++;      // front ++
+	if (shm_ptr->size_client_Q > 0) {
+		shm_ptr->size_client_Q--;      // size --
+		shm_ptr->front_client_Q++;      // front ++
 	}
 	else {
 		fprintf(stderr, "Cashier client queue is empty. Cannot dequeue\n");
 	}
 	/* release semaphore write lock after writing to shared memory */
-	if (sem_post(shared_mem_write_sem) == -1) {
+	if (sem_post(shm_write_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
@@ -192,22 +253,22 @@ void dequeue_client_cashier_q(struct Shared_memory_struct *shared_mem_ptr, sem_t
 /* func to dequeue client pid item in server FIFO queue
    This function automatically does a shared mem write semaphore lock
    IMPORTANT: This function should NEVER be called INSIDE the shared mem write semaphore lock state */
-void dequeue_client_server_q(struct Shared_memory_struct *shared_mem_ptr, sem_t *shared_mem_write_sem) {
+void dequeue_client_server_q(struct Shared_memory_struct *shm_ptr, sem_t *shm_write_sem) {
 	////////* Acquire semaphore lock first before writing in shared memory *////
 	////////////////////////////////////////////////////////////////////////////
-	if (sem_wait(shared_mem_write_sem) == -1) {
+	if (sem_wait(shm_write_sem) == -1) {
 		perror("sem_wait()");
 		exit(1);
 	}
-	if (shared_mem_ptr->size_server_Q > 0) {
-		shared_mem_ptr->size_server_Q--;  // size --
-		shared_mem_ptr->front_server_Q++;  // front ++
+	if (shm_ptr->size_server_Q > 0) {
+		shm_ptr->size_server_Q--;  // size --
+		shm_ptr->front_server_Q++;  // front ++
 	}
 	else {
 		fprintf(stderr, "Server client queue is empty. Cannot dequeue\n");
 	}
 	/* release semaphore write lock after writing to shared memory */
-	if (sem_post(shared_mem_write_sem) == -1) {
+	if (sem_post(shm_write_sem) == -1) {
 		perror("sem_post()");
 		exit(1);
 	}
